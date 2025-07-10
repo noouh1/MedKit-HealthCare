2c895ac9c2217c08f8c7e059457fd81ae5ba413a
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-07-10 19:40:45 +0300

    [Edit]:Resposive for small screen

--- src/pages/findDoctor/FindDoctor.tsx
+++ src/pages/findDoctor/FindDoctor.tsx
@@ -5,11 +5,35 @@ import Search from "../../components/search/Search.tsx";
 
 function FindDoctor() {
   return (
-    <Box sx={{  display: 'flex', flexDirection: 'column', background: '#f4f8fb' }}>
-      <Search />
-      <Box sx={{ flex: 1 }}>
-      <CardPreview />
+    <Box sx={{  
+      display: 'flex', 
+      flexDirection: 'column', 
+      background: '#f4f8fb',
+      minHeight: '100vh',
+      width: '100%',
+      overflowX: 'hidden'
+    }}>
+      {/* Search Section */}
+      <Box sx={{ 
+        width: '100%',
+        
+      }}>
+        <Search />
       </Box>
+      
+      {/* Main Content */}
+      <Box sx={{ 
+        flex: 1,
+        width: '100%',
+        px: { xs: 1, sm: 2, md: 3 },
+        py: { xs: 2, sm: 3 },
+        maxWidth: '100%',
+        boxSizing: 'border-box'
+      }}>
+        <CardPreview />
+      </Box>
+      
+      {/* Footer */}
       <Footer />
     </Box>
   );
